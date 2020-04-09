d57d07cf451611d7fe346b8effc573bab95962b9
tzlibre <tzlibre@protonmail.com>
2020-04-09 12:44:06 +0000

    eztzl

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./src/main.js",
+  mode: "production",
+  output: {
+    filename: "./eztz.min.js",
+  },
+  node: {
+    fs: "empty",
+  },
+  plugins: [
+    new CompressionPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
